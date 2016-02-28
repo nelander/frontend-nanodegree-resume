7527dc124ddfd604babb65eaa598961603fb66d5
Bengt Nelander <bengt@nelander.de>
2016-02-28 14:43:11 +0100

    resumeBuilder.js erweitert

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var myName="Bengt Nelander";
+var myRole="JavaScript Student";
+var formatedName=HTMLheaderName.replace("%data%",myName);
+var formatedRole=HTMLheaderRole.replace("%data%",myRole);
+$("#header").prepend(formatedRole);
+$("#header").prepend(formatedName);
